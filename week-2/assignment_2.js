a65b70f1ce388c40f1a99de7e1aa043e2dbb9f4a
Annie Chen <glianannie@gmail.com>
2022-01-11 18:12:55 +0800

    Finish w2 ass-2 : Object with (1)JSON (2)Class

--- week-2/assignment_2.js
+++ week-2/assignment_2.js
@@ -1,13 +1,13 @@
 function calculate(args){
-let result;
-if(args.op==="+"){
-result=args.n1+args.n2;
-}else if(args.op==="-"){
-result=args.n1-args.n2;
-}else{
-result="Not supported";
-}
-return result;
+	let result;
+	if(args.op==="+"){
+		result=args.n1+args.n2;
+	}else if(args.op==="-"){
+		result=args.n1-args.n2;
+	}else{
+		result="Not supported";
+	}
+	return result;
 }
 // Try to call calculate function correctly
 /*
@@ -20,3 +20,18 @@ add({n1:3, n2:4}); // your first way
 You should apply constructor or class as another way to create a proper object.
 add(用類別或建構式產生的物件); // your another way
 */
+
+//First way
+let args1 = {n1:1,n2:2,op:'+'}
+console.log(calculate(args1)); 
+
+//Second way
+class Arges {
+	constructor(n1,n2,op){
+		this.n1 = Number(n1);
+		this.n2 = Number(n2);
+		this.op = op;
+	}
+}
+let args2 = new Arges(5,1,'-');
+console.log(calculate(args2));
