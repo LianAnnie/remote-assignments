84f04bbafc09b83999dc710a27f368b3f876a544
Annie Chen <glianannie@gmail.com>
2022-01-12 17:57:19 +0800

    Revised w2 ass-4: DOM - Request1 - reclick to change text back

--- week-2/assignment_4/app.js
+++ week-2/assignment_4/app.js
@@ -1,3 +1,8 @@
-document.querySelector('h1').addEventListener('click',()=>{
-	document.querySelector('h1').innerHTML = `Have a Good Time!`;
+let h1 = document.querySelector('h1');
+h1.addEventListener('click',()=>{
+	if(h1.textContent == `Have a Good Time!`){
+		h1.innerHTML = `Welcome Message`;
+	}else{
+		h1.innerHTML = `Have a Good Time!`;
+	}
 });
